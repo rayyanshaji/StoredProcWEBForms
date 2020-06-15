518242029e3af33d484dca9cc022940346af969c
Mohammed Rayyan Shaji <rayyanz95@gmail.com>
2020-06-14 22:02:31 -0500

    first

---
+++ WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="StoredProcWebForm.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>World</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>Hello</p>
+        </div>
+        <input type="text" runat="server" class="form-control" id="PurchaseOrderID" placeholder="Enter" />
+        <asp:Button ID="btnSearch" runat="server" OnClick="btnSearch_Click" />
+        <asp:GridView ID="searchResults" runat="server">
+
+        </asp:GridView>
+    </form>
+
+
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace StoredProcWebForm
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            string connectionstring = ConfigurationManager.ConnectionStrings["StoredProc"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                SqlCommand sqlcom = new SqlCommand();
+                sqlcom.Connection = con;
+                sqlcom.CommandText = "GetTransactDetails";
+                sqlcom.CommandType = CommandType.StoredProcedure;
+                if (PurchaseOrderID.Value.Trim() != "")
+                {
+                    SqlParameter param = new SqlParameter("@PurchaseOrderID", PurchaseOrderID.Value);
+                    sqlcom.Parameters.Add(param);
+                }
+
+                con.Open();
+                SqlDataReader sd = sqlcom.ExecuteReader();
+                searchResults.DataSource = sd;
+                searchResults.DataBind();
+
+            }
+        }
+    }
+
+}
+
+     
+    
